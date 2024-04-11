ea8afce5b7fd8b1854b6c67cec0caf123ccdcc47
Orion Reed <orionreedt@gmail.com>
2024-04-11 05:08:16 -0700

    account for rotation

--- src/graph/GraphLayoutCollection.tsx
+++ src/graph/GraphLayoutCollection.tsx
@@ -1,6 +1,6 @@
 import { Layout } from 'webcola';
 import { BaseCollection } from '../collections/BaseCollection';
-import { Editor, TLArrowShape, TLShape, TLShapeId } from '@tldraw/tldraw';
+import { Editor, Geometry2d, TLArrowShape, TLShape, TLShapeId } from '@tldraw/tldraw';
 
 type ColaNode = {
   id: TLShapeId;
@@ -8,6 +8,7 @@ type ColaNode = {
   y: number;
   width: number;
   height: number;
+  rotation: number;
 };
 type ColaLink = {
   source: TLShapeId
@@ -15,18 +16,8 @@ type ColaLink = {
 };
 
 type LinkTargets = {
-  source: {
-    x: number
-    y: number
-    width: number
-    height: number
-  }
-  target: {
-    x: number
-    y: number
-    width: number
-    height: number
-  }
+  source: ColaNode
+  target: ColaNode
 }
 
 export class GraphLayoutCollection extends BaseCollection {
@@ -52,16 +43,13 @@ export class GraphLayoutCollection extends BaseCollection {
 
   override onAdd(shapes: TLShape[]) {
     super.onAdd(shapes);
-    const { colaNodes, colaLinks } = this.addShapes(shapes);
+    for (const shape of shapes) {
+      if (shape.type === "arrow")
+        this.addArrow(shape as TLArrowShape);
+      else
+        this.addGeo(shape);
 
-    for (const node of colaNodes) {
-      this.colaNodes.set(node.id, node);
     }
-
-    for (const link of colaLinks) {
-      this.colaLinks.add(link);
-    }
-
     this.updateGraphElements();
   }
 
@@ -90,62 +78,59 @@ export class GraphLayoutCollection extends BaseCollection {
     this.graphSim.start(1, 1, 1, 0, true, false);
     for (const node of this.graphSim.nodes() as ColaNode[]) {
 
+      const shape = this.editor.getShape(node.id);
+      const { w, h } = this.editor.getShapeGeometry(node.id).bounds
+      if (!shape) continue;
+
+      const { x, y } = getCornerToCenterOffset(w, h, shape.rotation);
+
       // Fix positions if we're dragging them
-      // TODO: avoid this expensive check
       if (this.editor.getSelectedShapeIds().includes(node.id)) {
-        const shape = this.editor.getShape(node.id);
-        if (!shape) continue;
-        node.x = shape.x + node.width / 2;
-        node.y = shape.y + node.height / 2;
+        node.x = shape.x + x;
+        node.y = shape.y + y;
       }
 
-      // Update shape sizes if they've changed
-      // TODO: avoid this expensive check
-      const geo = this.editor.getShapeGeometry(node.id);
-      node.width = geo.center.x * 2;
-      node.height = geo.center.y * 2;
+      // Update shape props
+      node.width = w;
+      node.height = h;
+      node.rotation = shape.rotation;
 
       // TODO: batch updates?
       this.editor.updateShape({
         id: node.id,
         type: "geo",
-        x: node.x - geo.center.x,
-        y: node.y - geo.center.y,
+        x: node.x - x,
+        y: node.y - y,
       });
     }
   };
 
-  addShapes = (shapes: TLShape[]) => {
-    const colaNodes: ColaNode[] = [];
-    const colaLinks: ColaLink[] = [];
 
-    for (const shape of shapes) {
-      if (shape.type === "arrow") {
-        const arrow = shape as TLArrowShape;
-        const source = arrow.props.start.type === 'binding' ? this.editor.getShape(arrow.props.start.boundShapeId) : undefined;
-        const target = arrow.props.end.type === 'binding' ? this.editor.getShape(arrow.props.end.boundShapeId) : undefined;
-        if (source && target) {
-          const link: ColaLink = {
-            source: source.id,
-            target: target.id
-          };
-          colaLinks.push(link);
-        }
-      } else {
-        const geo = this.editor.getShapeGeometry(shape);
-        const node: ColaNode = {
-          id: shape.id,
-          x: shape.x + geo.center.x,
-          y: shape.y + geo.center.y,
-          width: geo.center.x * 2,
-          height: geo.center.y * 2
-        };
-        colaNodes.push(node);
-      }
+
+  addArrow = (arrow: TLArrowShape) => {
+    const source = arrow.props.start.type === 'binding' ? this.editor.getShape(arrow.props.start.boundShapeId) : undefined;
+    const target = arrow.props.end.type === 'binding' ? this.editor.getShape(arrow.props.end.boundShapeId) : undefined;
+    if (source && target) {
+      const link: ColaLink = {
+        source: source.id,
+        target: target.id
+      };
+      this.colaLinks.add(link);
     }
+  }
 
-    return { colaNodes, colaLinks };
-  };
+  addGeo = (shape: TLShape) => {
+    const geo = this.editor.getShapeGeometry(shape);
+    const node: ColaNode = {
+      id: shape.id,
+      x: shape.x + geo.center.x,
+      y: shape.y + geo.center.y,
+      width: geo.center.x * 2,
+      height: geo.center.y * 2,
+      rotation: shape.rotation
+    };
+    this.colaNodes.set(shape.id, node);
+  }
 
   updateGraphElements() {
     const nodes = [...this.colaNodes.values()];
@@ -159,7 +144,7 @@ export class GraphLayoutCollection extends BaseCollection {
       .nodes(nodes)
       .links(links)
       .avoidOverlaps(true)
-      .linkDistance((edge) => this.calcEdgeDistance(edge as LinkTargets))
+      .linkDistance((edge) => calcEdgeDistance(edge as LinkTargets))
       .handleDisconnected(true)
     // .constraints(this.colaConstraints);
   }
@@ -178,32 +163,50 @@ export class GraphLayoutCollection extends BaseCollection {
     this.isRunning = false;
   }
 
-  calcEdgeDistance(edge: LinkTargets) {
-    const LINK_DISTANCE = 100;
-    // Calculate horizontal and vertical distances between centers
-    const dx = Math.abs(edge.target.x - edge.source.x);
-    const dy = Math.abs(edge.target.y - edge.source.y);
-
-    // Calculate edge-to-edge distances
-    const horizontalGap = Math.max(
-      0,
-      dx - (edge.source.width + edge.target.width) / 2
-    );
-    const verticalGap = Math.max(
-      0,
-      dy - (edge.source.height + edge.target.height) / 2
-    );
-
-    // Calculate straight-line distance between the centers of the nodes
-    const centerToCenterDistance = Math.sqrt(dx * dx + dy * dy);
-
-    // Adjust the distance by subtracting the edge-to-edge distance and adding the desired travel distance
-    const adjustedDistance = centerToCenterDistance -
-      Math.sqrt(horizontalGap * horizontalGap + verticalGap * verticalGap) +
-      LINK_DISTANCE;
-
-    return adjustedDistance;
-  };
 
 }
 
+function getCornerToCenterOffset(w: number, h: number, rotation: number) {
+
+  // Calculate the center coordinates relative to the top-left corner
+  const centerX = w / 2;
+  const centerY = h / 2;
+
+  // Apply rotation to the center coordinates
+  const rotatedCenterX = centerX * Math.cos(rotation) - centerY * Math.sin(rotation);
+  const rotatedCenterY = centerX * Math.sin(rotation) + centerY * Math.cos(rotation);
+
+  return { x: rotatedCenterX, y: rotatedCenterY };
+}
+
+function calcEdgeDistance(edge: LinkTargets) {
+  const LINK_DISTANCE = 100;
+
+  // horizontal and vertical distances between centers
+  const dx = edge.target.x - edge.source.x;
+  const dy = edge.target.y - edge.source.y;
+
+  // the angles of the nodes in radians
+  const sourceAngle = edge.source.rotation;
+  const targetAngle = edge.target.rotation;
+
+  // Calculate the rotated dimensions of the nodes
+  const sourceWidth = Math.abs(edge.source.width * Math.cos(sourceAngle)) + Math.abs(edge.source.height * Math.sin(sourceAngle));
+  const sourceHeight = Math.abs(edge.source.width * Math.sin(sourceAngle)) + Math.abs(edge.source.height * Math.cos(sourceAngle));
+  const targetWidth = Math.abs(edge.target.width * Math.cos(targetAngle)) + Math.abs(edge.target.height * Math.sin(targetAngle));
+  const targetHeight = Math.abs(edge.target.width * Math.sin(targetAngle)) + Math.abs(edge.target.height * Math.cos(targetAngle));
+
+  // Calculate edge-to-edge distances
+  const horizontalGap = Math.max(0, Math.abs(dx) - (sourceWidth + targetWidth) / 2);
+  const verticalGap = Math.max(0, Math.abs(dy) - (sourceHeight + targetHeight) / 2);
+
+  // Calculate straight-line distance between the centers of the nodes
+  const centerToCenterDistance = Math.sqrt(dx * dx + dy * dy);
+
+  // Adjust the distance by subtracting the edge-to-edge distance and adding the desired travel distance
+  const adjustedDistance = centerToCenterDistance -
+    Math.sqrt(horizontalGap * horizontalGap + verticalGap * verticalGap) +
+    LINK_DISTANCE;
+
+  return adjustedDistance;
+};
