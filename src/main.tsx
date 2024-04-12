0e08f334f116bb3af6afb6bce9b7e31baea5aed4
Orion Reed <orionreedt@gmail.com>
2024-04-12 02:14:47 -0700

    fix react root non-null assertion lint warning

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,12 @@
-import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "./css/index.css";
 
-ReactDOM.createRoot(document.getElementById("root")!).render(
-	<React.StrictMode>
+const rootElement = document.getElementById("root");
+if (rootElement) {
+	ReactDOM.createRoot(rootElement).render(
+		// <React.StrictMode>
 		<App />
-	</React.StrictMode>,
-);
+		// </React.StrictMode>
+	);
+}
